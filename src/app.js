366767f8f0da68797c51e6861cb90e2969396e1d
mizuki0201 <6m29uv7.tankss@gmail.com>
2022-11-26 20:41:27 +0900

    feat: hot loadingでjsファイルを読み込める状態まで完了

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>welcome!</h1>
+        <p>this is react development environment built by webpack</p>
+      </div>
+    );
+  }
+}
+
+export default App;
